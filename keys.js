3c92fe567879b0b77619ab08cf2928940f225ef2
Tyler Lackey <t-lack@Tylers-MacBook-Pro.local>
2017-07-08 13:12:50 -0500

    liri

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'Ulxmc5BLWUlsBh9UYzMooAN93',
+  consumer_secret: 'RIfVB1u8kL044LsCQOhSgNa8ryeYygRCwZ5isqgtEL0Ykj7bMU',
+  access_token_key: '883723756300099585-AxEDEzWoiJ15kPZULC3QuEXW92JpFUg',
+  access_token_secret: 'n30GmFgDV7CbN5GHkhffb2S5kZqp43DUQEnjDhiFZxm2Y',
+}
